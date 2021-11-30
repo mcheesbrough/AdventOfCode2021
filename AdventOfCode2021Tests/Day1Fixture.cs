ed8252ecfbff3a0026e38d49b3aa1eb830a3b0c0
Martin Cheesbrough <martin.cheesbrough@thetrainline.com>
2021-11-30 12:34:49 +0000

    Added test project

---
+++ AdventOfCode2021Tests/Day1Fixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2021Tests
+{
+    [TestFixture]
+    public class Day1Fixture
+    {
+        [Test]
+        public void FirstTestPlaceholder()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ AdventOfCode2021Tests/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.NUnit3;
+
+namespace AdventOfCode2021Tests
+{
+    public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoMoqDataAttribute(params object[] objects) : base(() => new Fixture()
+                .Customize(new AutoMoqCustomization { ConfigureMembers = true }),
+            objects)
+
+        { }
+    }
+}
